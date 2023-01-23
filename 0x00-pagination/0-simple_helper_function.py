c003c608b501a82a12f405757d161ddec4ff9f49
thala254 <thalamine@gmail.com>
2023-01-23 23:17:49 +0300

    added task files and a readme

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,6 +7,7 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    function that returns range of indexes to return in a list for the pagination prameters    
+    function that returns range of indexes to return in a list for
+    the pagination prameters
     """
     return (0 if page == 1 else (page - 1) * page_size, page * page_size)
